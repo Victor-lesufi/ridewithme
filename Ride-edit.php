6025e7d0f6e44e51d6a4f3b4c97ec00a6ea4e4e9
Victor-lesufi <100297610+Victor-lesufi@users.noreply.github.com>
2024-02-22 14:09:59 +0200

    Update Ride-edit.php

--- Ride-edit.php
+++ Ride-edit.php
@@ -12,66 +12,98 @@ require 'app/connnection.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>edit</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+
 </head>
 
+<style>
+    .bk {
+        background-color: lightseagreen;
+        padding: 10px;
+        line-height: 1px;
+    }
+</style>
+
 <body>
-    <div class="container">
-        <h4>ride edit
-            <a href="index.php">Back</a>
-        </h4>
+    <div class="bk">
+        <a href="ride.php" class="fs-1 fw-500 link-dark">&#8592;</a>
+        <div class="container">
+
+            <h4 class="text-center">
+                Edit Your Rides
+
+            </h4>
+        </div>
     </div>
+
     <div class="card-body">
         <?php
 
         if (isset($_GET['id'])) {
-            $student_id = mysqli_real_escape_string($con,$_GET['id']);
-            $query = "SELECT *  FROM test  WHERE id='$student_id'";
-            $query_run= mysqli_query($con,$query);
+            $ride_id = mysqli_real_escape_string($con, $_GET['id']);
+            $query = "SELECT *  FROM test  WHERE id='$ride_id'";
+            $query_run = mysqli_query($con, $query);
 
-            if(mysqli_num_rows($query_run) > 0){
+            if (mysqli_num_rows($query_run) > 0) {
 
-                $student = mysqli_fetch_array($query_run);
-              ?>
-              <form action="code.php" method="POST">
-              <input type="hidden" name="student_id" value= "<?=$student['id'];?>">
-            <div class="mb-3">
-                <label> name</label>
-                <input type="text" readonly name="name" value= "<?=$student['name']?>"class="form-control">
-            </div>
-            <div class="mb-3">
-                <label>Date</label>
-                <input type="date" name="lastname" value= "<?=$student['lastname']?>"class="form-control">
-                
-            </div>
-            <div class="mb-3">
-                <label>location</label>
-                <input type="text" name="location" value= "<?=$student['location']?>"class="form-control">
-                
-            </div>
-            <div class="mb-3">
-                <label>time</label>
-                <input type="time" name="time" value= "<?=$student['time']?>"class="form-control">
-                
-            </div>
-                <label>destination</label>
-                <input type="text" name="destination" value= "<?=$student['destination']?>"class="form-control">
-                
-            </div>
-                <label>price</label>
-                <input type="text" name="price" value= "<?=$student['price']?>"class="form-control">
-                
-            </div>
-            <button type="submit" name="update_student">update ride</button>
-        </form>
-              <?php
+                $ride = mysqli_fetch_array($query_run);
+        ?>
+                <form action="code.php" method="POST">
+                    <input type="hidden" name="ride_id" value="<?= $ride['id']; ?>">
+                    <div class="mb-3">
+                        <label> Name:</label>
+                        <input type="text" readonly name="name" value="<?= $ride['name'] ?>" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label>Date:</label>
+                        <input type="date" name="date" value="<?= $ride['date'] ?>" class="form-control">
 
-            }else{
+                    </div>
+                    <div class="mb-3">
+                        <label>Location:</label>
+                        <input type="text" name="location" value="<?= $ride['location'] ?>" class="form-control">
+
+                    </div>
+                    <div class="mb-3">
+                        <label>Time:</label>
+                        <input type="time" name="time" value="<?= $ride['time'] ?>" class="form-control">
+
+                    </div>
+                    <div class="mb-3">
+                        <label>Destination:</label>
+                        <input type="text" name="destination" value="<?= $ride['destination'] ?>" class="form-control">
+
+                    </div>
+                    <div class="mb-3">
+                        <label>type:</label>
+                        <select class="form-select" aria-label="Default select example" name="type">
+                            <option value="<?= $ride['type'] ?>"><?= $ride['type'] ?></option>
+                            <option value="transport">transport</option>
+                            <option value="lift">lift</option>
+
+                        </select>
+
+                    </div>
+                    <div class="mb-3">
+                        <label>Price:</label>
+                        <input type="number" name="price" value="<?= $ride['price'] ?>" class="form-control">
+
+                    </div>
+                    <div class="mb-3">
+
+                        <button class="btn btn-primary" type="submit" name="update_ride">Update ride</button>
+                    </div>
+                </form>
+        <?php
+
+            } else {
                 echo "<h4>no  such records</h4>";
             }
         }
 
         ?>
-       
+
     </div>
 </body>
 
